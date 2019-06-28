7ac8d292f7e7326ccc0fbd2a09088b241be98530
mahonggang <1584157651@qq.com>
2019-06-28 20:42:50 +0800

    first commit

---
+++ src/main/java/com/sc/app/SCAppApplication.java
@@ -0,0 +1,37 @@
+package com.sc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class SCAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SCAppApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SCAppApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public RestTemplate restTemplateOther() {
+		return new RestTemplate();
+	}
+}
