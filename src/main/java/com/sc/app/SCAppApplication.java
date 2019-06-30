28be8d0160036292e39c8736f8b3aa885d54ae9c
mahonggang <1584157651@qq.com>
2019-06-30 11:17:16 +0800

    qaza

--- src/main/java/com/sc/app/SCAppApplication.java
+++ src/main/java/com/sc/app/SCAppApplication.java
@@ -3,6 +3,7 @@ package com.sc.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -10,9 +11,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.sc.app.config.FileConfig;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableConfigurationProperties({ FileConfig.class })
 public class SCAppApplication extends SpringBootServletInitializer {
 
 	@Override
